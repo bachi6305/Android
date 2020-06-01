10191bc2b82445c2113c3e614092feb80ea09ae5
bachi6305 <bachi6305@gmail.com>
2020-06-01 15:06:50 -0700

    updating

--- AndroidPractice/app/src/main/java/com/example/androidpractice/MainActivity.java
+++ AndroidPractice/app/src/main/java/com/example/androidpractice/MainActivity.java
@@ -9,10 +9,14 @@ import java.util.logging.Logger;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String CLASS_NAME = MainActivity.class.getSimpleName();
+    public Logger logger = Logger.getLogger(CLASS_NAME);
+
     TextView textView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        logger.info(CLASS_NAME);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
